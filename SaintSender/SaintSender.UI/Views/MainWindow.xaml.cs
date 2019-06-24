a5b6761136d5f2700b6f94e77de70a7b07664fc9
hudiM <deadcsavo@gmail.com>
2019-06-24 15:04:07 +0200

    Inbox view ButtonMenu view Single Mail Display view Basic mail models Some layout designs Gmail connection Gmail request mails returns data

--- SaintSender/SaintSender.UI/Views/MainWindow.xaml.cs
+++ SaintSender/SaintSender.UI/Views/MainWindow.xaml.cs
@@ -1,28 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Models.Logic;
+using Models.Models;
+using SaintSender.UI.Utils;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SaintSender.UI.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private MailModel _selectedMail = new MailModel();
+
+        public ICommand ChangeSelectedMailCommand { get; set; }
+
+        public ObservableCollection<MailModel> Mails { get; set; } = new ObservableCollection<MailModel>();
+
+        public MailModel SelectedMail
+        {
+            get => _selectedMail; set
+            {
+                _selectedMail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void ChangeSelectedMail(object o)
+        {
+            SelectedMail = Mails[0];
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            Debug();
+            ChangeSelectedMailCommand = new RelayCommand(ChangeSelectedMail);
+            DataContext = this;
+        }
+
+        public void Debug()
+        {
+            MailRepository mailRepo = new MailRepository();
+            Console.WriteLine(mailRepo.GetAllMails());
+
+            MailModel mail;
+            for (int i = 0; i < 5; i++)
+            {
+                mail = new MailModel();
+                mail.Sender = "sender " + i;
+                mail.Message = "msg " + i;
+                mail.Subject = "subject " + i;
+                mail.Date = DateTime.Now;
+                Mails.Add(mail);
+            }
         }
     }
 }
