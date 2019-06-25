1f8d633f9fd6a533e4f34935d409ae23e81c8b2f
Ács Bence Tamás <acs.bence.tamas@gmail.com>
2019-06-25 12:37:45 +0200

    Merge

--- SaintSender/SaintSender.BackendTests/Models/ConfigHandlerTests.cs
+++ SaintSender/SaintSender.BackendTests/Models/ConfigHandlerTests.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.IO.IsolatedStorage;
 
-namespace Models.Models.Tests
+namespace SaintSender.Backend.Models.Tests
 {
     [TestClass()]
     public class ConfigHandlerTests
