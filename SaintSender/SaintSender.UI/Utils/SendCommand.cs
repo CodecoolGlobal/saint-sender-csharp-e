6c84d85647660577bfb9ead533ff401a22751c45
Benjamin Kovács <kojokobe@gmail.com>
2019-06-26 10:07:02 +0200

    Added SendCommand

--- SaintSender/SaintSender.UI/Utils/SendCommand.cs
+++ SaintSender/SaintSender.UI/Utils/SendCommand.cs
@@ -1,24 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SaintSender.UI.Utils
 {
     class SendCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public SendCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
 
         public bool CanExecute(object parameter)
         {
-            throw new NotImplementedException();
+            return this.canExecute == null || this.canExecute(parameter);
         }
 
         public void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            execute(parameter);
         }
     }
 }
