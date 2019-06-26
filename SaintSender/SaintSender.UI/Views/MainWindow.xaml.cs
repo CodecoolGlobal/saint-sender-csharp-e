5377cc5f8c6d0193345a5c23803db598a8070186
hudiM <deadcsavo@gmail.com>
2019-06-26 12:54:45 +0200

    internet connection test regions for readablility

--- SaintSender/SaintSender.UI/Views/MainWindow.xaml.cs
+++ SaintSender/SaintSender.UI/Views/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using SaintSender.UI.Utils;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Net;
 using System.Net.Mail;
 using System.Runtime.CompilerServices;
 using System.Windows;
@@ -18,11 +19,13 @@ namespace SaintSender.UI.Views
         private MailModel _selectedMail = new MailModel();
         private LoginConfig _loginConfigWindow;
 
-        public ICommand ChangeSelectedMailCommand { get; set; }
+        #region ICommands
         public ICommand SaveMailsToStorageCommand { get; set; }
         public ICommand LoadMailsFromStorageCommand { get; set; }
         public ICommand LoadMailsFromServerCommand { get; set; }
+        #endregion
 
+        #region Properties
         public ObservableCollection<MailModel> Mails { get; set; } = new ObservableCollection<MailModel>();
 
         public ICommand SignInCommand { get; set; }
@@ -39,19 +42,36 @@ namespace SaintSender.UI.Views
                 OnPropertyChanged();
             }
         }
+        #endregion
 
+        #region Property change handler
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
 
-        public void ChangeSelectedMail(object o)
+        #region Command methods
+        private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
         {
-            SelectedMail = Mails[0];
+            Environment.Exit(0);
         }
 
+        private void ShowSignInWindow(object param)
+        {
+            if (!_loginConfigWindow.IsLoaded)
+            {
+                _loginConfigWindow = GetLoginConfig();
+            }
+            _loginConfigWindow.Show();
+        }
+
+        /// <summary>
+        /// Saves mails to storage for the current user
+        /// </summary>
+        /// <param name="o"></param>
         public void SaveMailsToStorage(object o)
         {
             MailAddress address = new MailAddress(Repository.login);
@@ -63,7 +83,7 @@ namespace SaintSender.UI.Views
         }
 
         /// <summary>
-        /// Gets mails from local storage
+        /// Gets mails from local storage for the current user
         /// </summary>
         /// <param name="o"></param>
         public void LoadMailsFromStorage(object o)
@@ -85,7 +105,7 @@ namespace SaintSender.UI.Views
         }
 
         /// <summary>
-        /// Gets mails from remote server
+        /// Gets mails from remote server for the current user
         /// </summary>
         /// <param name="o"></param>
         public void LoadMailsFromServer(object o)
@@ -116,10 +136,7 @@ namespace SaintSender.UI.Views
                 Mails.Add(item);
             }
         }
-        private void MainWindow_Unloaded(object sender, RoutedEventArgs e)
-        {
-            Environment.Exit(0);
-        }
+        #endregion
 
         public MainWindow()
         {
@@ -134,27 +151,38 @@ namespace SaintSender.UI.Views
             // Debug
 #warning debug repo, remove once login fixed
             Repository = new MailRepository();
+            Console.WriteLine(CheckForInternetConnection());
 
             // Setup commands
             SignInCommand = new RelayCommand(ShowSignInWindow);
-            ChangeSelectedMailCommand = new RelayCommand(ChangeSelectedMail);
             SaveMailsToStorageCommand = new RelayCommand(SaveMailsToStorage);
             LoadMailsFromStorageCommand = new RelayCommand(LoadMailsFromStorage);
             LoadMailsFromServerCommand = new RelayCommand(LoadMailsFromServer);
         }
 
-        private void ShowSignInWindow(object param)
-        {
-            if (!_loginConfigWindow.IsLoaded)
-            {
-                _loginConfigWindow = GetLoginConfig();
-            }
-            _loginConfigWindow.Show();
-        }
-
         private LoginConfig GetLoginConfig()
         {
             return new LoginConfig(Config, Repository);
+        }
+
+        /// <summary>
+        /// Tries to connect to test website to verify whether there is internet connection
+        /// </summary>
+        /// <returns>True if connection was succesful</returns>
+        private static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead("http://clients3.google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
